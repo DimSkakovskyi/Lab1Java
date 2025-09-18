1a8e8b4d599c5cc22542391416b4a3018818cd08
DimSkakovskyi <dimonskak@ukr.net>
2025-09-18 14:08:07 +0300

    Refactoring code, because previous inheritances were unnecessary. Created CandyType through enum, to identify type of candy, without additional classes (like in previous version). CandyWeight should be done next

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<SweetBase> sweets = new ArrayList<>();
-        sweets.add(new Candy("ChocoCandy", 25, 40, 12, "chocolate"));
-        sweets.add(new Chocolate("Dark Choco", 100, 30, 50, "dark"));
-        sweets.add(new Cookie("Oreo", 50, 35, 20, true));
-        sweets.add(new Marshmallow("Zephyr", 30, 60, 15, "white"));
 
-        for (SweetBase sweet : sweets) {
-            System.out.println(sweet + " → " + sweet.getDescription());
-        }
     }
 }
---
+++ src/candy_properties/CandyType.java
@@ -0,0 +1,6 @@
+package candy_properties;
+
+public enum CandyType {
+    CHOCOLATE, CARAMEL, IRIS, JELLY, MARSHMALLOW
+}
+
